8e7e62e626b2f12cf6fb38157c5713f93b27ba09
muchoki_esther <muchokiesther8@gmail.com>
2023-08-20 20:22:59 +0300

    Establishing Connection

---
+++ ef-learning/PublisherData/PubContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PublisherDomian;
+
+namespace PublisherData
+{
+    public class PubContext:DbContext
+    {
+        public DbSet <Author> Authors { get; set; }
+        public DbSet<Book> Books { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server = DESKTOP-Q8FU87I\\SQLEXPRESS; database = Publisher; TrustServerCertificate = true; Trusted_Connection = true; user Id  = sa; password = wamuyu@2023 ") ;//connecting to my database
+
+        }
+    }
+}
---
+++ ef-learning/ef-learning/Program.cs
@@ -0,0 +1,18 @@
+﻿
+using PublisherData;
+
+
+using (PubContext context = new PubContext())
+{
+    context.Database.EnsureCreated();
+}
+GetAuthors();
+
+void GetAuthors()
+{
+    using var context = new PubContext();
+    var authors=context.Authors.ToList(); // creates a LINQ (language Intergrated query)
+    foreach (var author in authors){
+        Console.WriteLine(author.FirstName + " " + author.LastName);
+    }
+}
